fb1009b24d462ad2ec518a25ac7c573125529baa
JohnReyGimenez <gimenezjr5@gmail.com>
2025-08-07 21:19:52 +0800

    added: paged and routing

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-const MovieDetails = () => {
+const profile = () => {
   return (
     <View>
-      <Text>[id]</Text>
+      <Text>profile</Text>
     </View>
   )
 }
 
-export default MovieDetails
+export default profile
